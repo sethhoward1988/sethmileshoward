850343b0ff5fdd54abdafdfc5e11f63aac2fa571
Seth Howard <smiles.seth@gmail.com>
2013-08-18 19:46:19 -0500

    removed files

--- blog/wp-content/themes/blass2/footer.php
+++
@@ -1,7 +0,0 @@
-<div id="footer">
-
-
-
-        <p>&copy; <?php echo date("Y")?> <!-- Please leave this line intact --><?php if (is_home()) : ?><?php bloginfo('name'); ?> | Theme <a href="http://1000ff.de/wordpress-theme-blass-english-version/">Blass</a> by <a href="http://1000ff.de/">1000ff</a><?php else : ?>Theme Blass by 1000ff<?php endif; ?> | Powered by <a href="http://wordpress.org/">WordPress</a></p>
-
-</div>
